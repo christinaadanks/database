4d35f22d6e435b5bf65086c50d37fd663f8bddca
christinaathecoder <christinaa.danks@me.com>
2022-08-02 20:35:30 -0700

    Updated user

--- web_app/src/main/java/servlets/SelectServlet.java
+++ web_app/src/main/java/servlets/SelectServlet.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Locale;
 
 /**
- * @author christinaadanks
+ * @author christinaathecoder
  */
 
 @WebServlet(name = "SelectServlet", value = "/SelectServlet")
